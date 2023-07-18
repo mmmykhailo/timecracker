eee7979e05e47e7b73efecbf87d54d2370abf203
OleksiiMehlii <oleksii.mehlii@ukad-group.com>
2023-07-18 15:53:30 +0300

    Test update

--- renderer/components/ActivitiesSection.tsx
+++ renderer/components/ActivitiesSection.tsx
@@ -32,7 +32,7 @@ export default function ActivitiesSection({
           className="block px-4 py-4 text-sm font-medium text-center text-gray-500 bg-gray-50 hover:text-gray-700 sm:rounded-b-lg"
           onClick={() => onEditActivity("new")}
         >
-          Track more time
+          Track more time test test test
         </a>
       </div>
     </div>
